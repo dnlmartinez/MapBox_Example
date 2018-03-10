66014ff30709ec76a608b1931afd0050fd70e87f
daniel martinez gonzalez <dnlmartinezgonzalez@gmail.com>
2018-03-10 18:27:22 +0100

    navigation when you tap on a annotation in a map with a long press gesture

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -8,8 +8,15 @@
 import UIKit
 import Mapbox
 
+import MapboxCoreNavigation
+import MapboxNavigation
+import MapboxDirections
+
+
 class ViewController: UIViewController , MGLMapViewDelegate{
 
+    var mapView: NavigationMapView!
+    var directionsRoute: Route?
     
     let coordinates = [
                         CLLocationCoordinate2D(latitude: 42.213762, longitude: -8.741640),
@@ -36,15 +43,17 @@ class ViewController: UIViewController , MGLMapViewDelegate{
         //let url = URL(string: "mapbox://styles/mapbox/satellite-streets-v10")
         let url = URL(string: "mapbox://styles/mapbox/dark-v9")
 
-        let mapView = MGLMapView(frame: view.bounds, styleURL: url)
-        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        //mapView.setCenter(CLLocationCoordinate2D(latitude: 42.213762, longitude: -8.739640), zoomLevel: 15, animated: false)
         
+        mapView = NavigationMapView(frame: view.bounds, styleURL: url)
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         mapView.camera = MGLMapCamera(lookingAtCenter: CLLocationCoordinate2D(latitude: 42.213762, longitude:  -8.739640), fromDistance: 600, pitch: 45, heading: 200)
         
         mapView.delegate = self
-        view.addSubview(mapView)
         
+        mapView.showsUserLocation = true
+        mapView.setUserTrackingMode(.follow, animated: true)
+        
+        view.addSubview(mapView)
         
         var pointAnnotations = [MGLPointAnnotation]()
         for coordinate in coordinates {
@@ -54,9 +63,74 @@ class ViewController: UIViewController , MGLMapViewDelegate{
             pointAnnotations.append(point)
         }
         
+        let setDestination = UILongPressGestureRecognizer(target: self, action: #selector(didLongPress(_:)))
+        
+        
         mapView.addAnnotations(pointAnnotations)
+        mapView.addGestureRecognizer(setDestination)
+        
+    
     }
     
+    func calculateRoute(from origin: CLLocationCoordinate2D, to destination: CLLocationCoordinate2D, completion: @escaping (Route?, Error?) -> ()) {
+        
+        let origin = Waypoint(coordinate: origin, coordinateAccuracy: -1, name: "Start")
+        let destination = Waypoint(coordinate: destination, coordinateAccuracy: -1, name: "Finish")
+        let options = NavigationRouteOptions(waypoints: [origin, destination], profileIdentifier: .automobileAvoidingTraffic)
+        
+        _ = Directions.shared.calculate(options) { [unowned self] (waypoints, routes, error) in
+            self.directionsRoute = routes?.first
+            
+        self.drawRoute(route: self.directionsRoute!)
+        }
+    }
+    
+    
+    func drawRoute(route: Route) {
+        guard route.coordinateCount > 0 else { return }
+        
+        var routeCoordinates = route.coordinates!
+        let polyline = MGLPolylineFeature(coordinates: &routeCoordinates, count: route.coordinateCount)
+        
+        if let source = mapView.style?.source(withIdentifier: "route-source") as? MGLShapeSource {
+            source.shape = polyline
+        } else {
+            let source = MGLShapeSource(identifier: "route-source", features: [polyline], options: nil)
+            
+            let lineStyle = MGLLineStyleLayer(identifier: "route-style", source: source)
+            lineStyle.lineColor = MGLStyleValue(rawValue: #colorLiteral(red: 0.1897518039, green: 0.3010634184, blue: 0.7994888425, alpha: 1))
+            lineStyle.lineWidth = MGLStyleValue(rawValue: 3)
+            
+            mapView.style?.addSource(source)
+            mapView.style?.addLayer(lineStyle)
+        }
+    }
+    
+    
+    
+    @objc func didLongPress(_ sender: UILongPressGestureRecognizer) {
+        guard sender.state == .began else { return }
+        
+        // Converts point where user did a long press to map coordinates
+        let point = sender.location(in: mapView)
+        let coordinate = mapView.convert(point, toCoordinateFrom: mapView)
+        
+        // Create a basic point annotation and add it to the map
+        let annotation = MGLPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = "Start navigation"
+        mapView.addAnnotation(annotation)
+        
+        calculateRoute(from: (mapView.userLocation!.coordinate), to: annotation.coordinate) { (route, error) in
+            if error != nil {
+                print("Error calculating route")
+            }
+        }
+
+    }
+    
+    
+    ///MARK: MAPBOX DELEGATE METHODS
     
     func mapView(_ mapView: MGLMapView, viewFor annotation: MGLAnnotation) -> MGLAnnotationView? {
         
@@ -121,6 +195,12 @@ class ViewController: UIViewController , MGLMapViewDelegate{
     
     func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
         return true
+    }
+    
+    
+    func mapView(_ mapView: MGLMapView, tapOnCalloutFor annotation: MGLAnnotation) {
+        let navigationViewController = NavigationViewController(for: directionsRoute!)
+        self.present(navigationViewController, animated: true, completion: nil)
     }
     
 }
